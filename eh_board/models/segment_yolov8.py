68c91ffb848a370f3914aff4003b0154cad5a209
Pasindu <pasindusandakan@gmail.com>
2024-09-07 20:51:52 +0530

    updated

--- eh_board/models/segment_yolov8.py
+++ eh_board/models/segment_yolov8.py
@@ -10,5 +10,5 @@ if os.path.exists(IMAGE_SAVE_DIR):
     shutil.rmtree(IMAGE_SAVE_DIR)
     
 def seg_image(image_path, save=True):
-        results = model.predict(image_path, save=True)
+        results = model.predict(image_path, save=save)
         return results
